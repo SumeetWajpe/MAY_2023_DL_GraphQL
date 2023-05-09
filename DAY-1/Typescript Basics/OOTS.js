e85e32520f97fe1559875c8fc834a35bb54d971c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-09 12:36:28 +0530

    📝 Spread Operator - Arrays

--- DAY-1/Typescript Basics/OOTS.js
+++ DAY-1/Typescript Basics/OOTS.js
@@ -2,6 +2,15 @@
 //   name: string;
 //   country: string;
 // }
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // var player:IPlayer = { name: "Djokovic", country: "Serbia", xyz: 123 };
 // type IPlayer = {
 //   name: string;
@@ -9,17 +18,28 @@
 // };
 // var player: IPlayer = { name: "Djokovic", country: "Serbia", xyz: 123 };
 //  Destructuring - Arrays
-var cars = ["BMW", "AUDI", "MERC"];
+// var cars: string[] = ["BMW", "AUDI", "MERC"];
 // let firstCar = cars[0];
 // let secondCar = cars[1];
 // OR
-var firstCar = cars[0], secondCar = cars[2];
-console.log(firstCar);
-console.log(secondCar);
+// let [firstCar, , secondCar] = cars;
+// console.log(firstCar);
+// console.log(secondCar);
 //  Destructuring - Objects
-var player = { fname: "Djokovic", country: "Serbia", isWorldNo1: true };
+// var player = {
+//   fname: "Djokovic",
+//   country: "Serbia",
+//   isWorldNo1: true,
+//   nickname: "GOAT",
+// };
 // let fname = player.fname;
 // let country = player.country;
 // OR
-var country = player.country, fname = player.fname;
-console.log(fname);
+// let { country, fname, ...restProps } = player;
+// console.log(fname);
+// console.log(restProps);
+// Spread Operator - Arrays
+var cars = ["BMW", "AUDI", "MERC"];
+var moreCars = ["TATA", "MAHINDRA"];
+var allCars = __spreadArray(__spreadArray([], cars, true), moreCars, true);
+console.log(allCars);
--- DAY-1/Typescript Basics/OOTS.ts
+++ DAY-1/Typescript Basics/OOTS.ts
@@ -13,18 +13,30 @@
 // var player: IPlayer = { name: "Djokovic", country: "Serbia", xyz: 123 };
 
 //  Destructuring - Arrays
-var cars: string[] = ["BMW", "AUDI", "MERC"];
+// var cars: string[] = ["BMW", "AUDI", "MERC"];
 // let firstCar = cars[0];
 // let secondCar = cars[1];
 // OR
-let [firstCar, , secondCar] = cars;
-console.log(firstCar);
-console.log(secondCar);
+// let [firstCar, , secondCar] = cars;
+// console.log(firstCar);
+// console.log(secondCar);
 
 //  Destructuring - Objects
-var player = { fname: "Djokovic", country: "Serbia", isWorldNo1: true };
+// var player = {
+//   fname: "Djokovic",
+//   country: "Serbia",
+//   isWorldNo1: true,
+//   nickname: "GOAT",
+// };
 // let fname = player.fname;
 // let country = player.country;
 // OR
-let { country, fname } = player;
-console.log(fname);
+// let { country, fname, ...restProps } = player;
+// console.log(fname);
+// console.log(restProps);
+
+// Spread Operator - Arrays
+var cars: string[] = ["BMW", "AUDI", "MERC"];
+var moreCars: string[] = ["TATA", "MAHINDRA"];
+var allCars: string[] = [...cars, ...moreCars];
+console.log(allCars);
