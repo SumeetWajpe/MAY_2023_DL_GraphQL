1c1799be14145a5ba2686d3da4b01d98563c67c2
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-15 12:25:10 +0530

    📝 Fetching data from the cache

--- DAY-5/React-ApolloClient/onlinetraining-client/src/coursedetails/coursedetails.component.jsx
+++ DAY-5/React-ApolloClient/onlinetraining-client/src/coursedetails/coursedetails.component.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "..";
+import { GET_ALL_COURSES } from "../graphql/querries";
 
 export default function CourseDetails() {
-  return <div>Course Details</div>;
+  let [course, setCourse] = useState({});
+  const { cid } = useParams();
+  useEffect(() => {
+    //fetch data from cache
+    const { courses } = client.readQuery({ query: GET_ALL_COURSES }); //any previously executed query
+    let currentCourse = courses.find(c => c.id == cid);
+    setCourse(currentCourse);
+  });
+  return <div>Course Details for {course.title}</div>;
 }
--- DAY-5/React-ApolloClient/onlinetraining-client/src/index.js
+++ DAY-5/React-ApolloClient/onlinetraining-client/src/index.js
@@ -9,7 +9,7 @@ import {
   gql,
 } from "@apollo/client";
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
