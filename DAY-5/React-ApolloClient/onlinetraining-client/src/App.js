cc3d558563f954eb5fda017c50d0176dcbe83c2b
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-15 12:52:02 +0530

    📝 Added Course Details & navbar to App.js

--- DAY-5/React-ApolloClient/onlinetraining-client/src/App.js
+++ DAY-5/React-ApolloClient/onlinetraining-client/src/App.js
@@ -1,11 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Listofcourses from "./listofcourses/listofcourses.component";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import CourseDetails from "./coursedetails/coursedetails.component";
 function App() {
   return (
     <BrowserRouter>
+      <nav className="navbar  navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Online shopping
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Courses
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/newcourse">
+                  New Course
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <Routes>
         <Route path="/" element={<Listofcourses></Listofcourses>}></Route>
         <Route path="/coursedetails/:cid" element={<CourseDetails />}></Route>
--- DAY-5/React-ApolloClient/onlinetraining-client/src/coursedetails/coursedetails.component.jsx
+++ DAY-5/React-ApolloClient/onlinetraining-client/src/coursedetails/coursedetails.component.jsx
@@ -12,5 +12,29 @@ export default function CourseDetails() {
     let currentCourse = courses.find(c => c.id == cid);
     setCourse(currentCourse);
   });
-  return <div>Course Details for {course.title}</div>;
+  return (
+    <div className="row m-2">
+      <div className="col-md-9">
+        <img
+          src={course.imageUrl}
+          className="card-img-top"
+          alt={course.title}
+        />
+      </div>
+      <div className="col-md-3 border-start border-secondary">
+        <h2>{course.title}</h2>
+        <div className="d-flex gap-1 align-items-center">
+          <img
+            src={course.trainer?.avatarUrl}
+            className="rounded"
+            alt={course.title}
+            width="50px"
+            height="50px"
+          />
+          <strong>{course.trainer?.name}</strong>
+        </div>
+        <p className="card-text">₹. {course.price}</p>
+      </div>
+    </div>
+  );
 }
